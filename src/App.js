d9652b81067e6bbdef5d1a6b64682b50bf94acde
indra <indrasena197@gmail.com>
2024-05-23 11:13:00 +0530

    ee

---
+++ src/App.js
@@ -0,0 +1,21 @@
+ import Statistics from './component/statistics'
+ import TransactionsTable from './component/table'
+ import  BarChartComponent from './component/barchart'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <TransactionsTable/>
+      <Statistics/>
+      < BarChartComponent/>
+    
+
+
+    </>
+  )
+}
+
+export default App
